0f0719806e29dc3a7e7dd8c59e3f9cb8eb4e9bbd
Tim Eichinger <privat.timeichinger@gmail.com>
2020-11-12 14:10:46 +0100

    updated version task5

--- exercise_1/task5.py
+++ exercise_1/task5.py
@@ -3,30 +3,15 @@ TABLE = [[34, 234, 2],
         [6, 234, 8],
         [9, 1340, 11]]
 
-def print_table():
-    for i in range(len(TABLE)):
-        for j in range(len(TABLE[0])):
-            print(" " + str(TABLE[i][j]), end=",")
-        print()
 def input_calculate():
     search = input("Gib die Nummer ein die du suchen willst: ")
-
-    col = 1
-    row = 1
+    
     counter = 0
-    found = False
+
     for i in range(len(TABLE)):
         for j in range(len(TABLE[0])):
             if (counter == int(search)):
-                print("Zeile: " + str(row) + ", Spalte " + str(col))
-                found = True
-                break
+                print('Zeile: {}, Spalte: {}'.format(i+1, j+1))
             counter += 1
-            col += 1
-        if (found == True):
-             break
-        row += 1
-        col = 1
 
-print_table()
 input_calculate()
