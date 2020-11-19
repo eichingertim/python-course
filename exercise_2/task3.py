62a6b86912256cd580bdb63a3e4e1b62f4d70d8b
Timon Lorenz <TimmensOne@users.noreply.github.com>
2020-11-19 09:48:39 +0100

    typos removed

--- exercise_2/task3.py
+++ exercise_2/task3.py
@@ -12,6 +12,6 @@ equation = x**2 + 4*x + 4
 solution = 0
 
 if (equation == solution):
-    print("your input is not a solution for 'x^2 + 4x + 4 = 0'")
-else:
     print("your input is a solution for 'x^2 + 4x + 4 = 0'")
+else:
+    print("your input is not a solution for 'x^2 + 4x + 4 = 0'")
