a8699efdcd03fcd36e426f2e928cdb73ca789199
Tim Eichinger <privat.timeichinger@gmail.com>
2020-11-20 13:20:11 +0100

    added exercise 3 task 1

---
+++ exercise_3/task1.py
@@ -0,0 +1,36 @@
+import math
+import cmath
+
+def read_in_variable(text):
+    variable = ""
+    while (not variable.isdigit()):
+        variable = input(text)
+        try:
+            variable = float(variable)
+            return variable
+        except ValueError:
+            print("This was not a number. Try again!")
+
+def calculate_zero_places(a, b, c, is_complex):
+    x1, x2 = [0,0]
+    if is_complex:
+        x1 = ((-b) + cmath.sqrt(b*b-4*a*c))/(2*a)
+        x2 = ((-b) - cmath.sqrt(b*b-4*a*c))/(2*a)
+    else:
+        x1 = ((-b) + math.sqrt(b*b-4*a*c))/(2*a)
+        x2 = ((-b) - math.sqrt(b*b-4*a*c))/(2*a)
+    return (x1, x2)
+
+a = read_in_variable("Enter a digit for a: ")
+b = read_in_variable("Enter a digit for b: ")
+c = read_in_variable("Enter a digit for c: ")
+
+zp = calculate_zero_places(a, b, c, False)
+zpc = calculate_zero_places(a, b, c, True)
+
+print('Relle Zahlen x1: {:.3f}, x2: {:.3f}'.format(zp[0], zp[1]))
+print('Komplexe Zahlen x1: {:.3f}, x2: {:.3f}'.format(zpc[0], zpc[1]))
+
+
+
+
